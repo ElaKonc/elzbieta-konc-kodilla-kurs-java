09103d24c3609d3a4d085ca9bf99cdd75f204a4b
Elzbieta <you@example.com>
2020-03-22 17:15:01 +0100

    uzupełnienie klasy shapeCollector

--- kodillaa-testing/src/main/java/com/kodilla/testing/shape/ShapeCollector.java
+++ kodillaa-testing/src/main/java/com/kodilla/testing/shape/ShapeCollector.java
@@ -7,20 +7,27 @@ public class ShapeCollector {
     List<Shape> shapes = new ArrayList<Shape>();
 
     public void addFigure(Shape shape) {
-
+        shapes.add(shape);
     }
 
     public void removeFigure(Shape shape) {
-
+        shapes.remove(shape);
     }
 
     public Shape getFigure(int n){
-        return null;
+        Shape theFigure = null;
+        if(n>=0 && n<shapes.size()) {
+            theFigure = shapes.get(n);
+        }
+        return theFigure;
     }
 
     public List<Shape> showFigures() {
-        return null;
+        return shapes;
     }
 
-
+    public Integer getShapesSize () {
+        int size = shapes.size();
+        return size;
+    }
 }
