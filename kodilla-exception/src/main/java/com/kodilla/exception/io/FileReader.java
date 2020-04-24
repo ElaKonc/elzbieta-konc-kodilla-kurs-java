dbddcb1c67bb8da2059ff6c6a2d7b25eaaa199e9
Elzbieta <you@example.com>
2020-04-24 09:43:50 +0200

    wyjątki

---
+++ kodilla-exception/src/main/java/com/kodilla/exception/io/FileReader.java
@@ -0,0 +1,22 @@
+package com.kodilla.exception.io;
+
+import java.io.File;
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+import java.util.stream.Stream;
+
+public class FileReader {
+    public void readFile() {
+        ClassLoader classLoader = getClass().getClassLoader();
+        File file = new File(classLoader.getResource("file/names.txt").getFile());
+        Path path = Paths.get("file/name.txt");
+
+        try {
+            Stream<String> fileLines = Files.lines(path);
+        } catch (IOException e) {
+            System.out.println("Wystąpił błąd");
+        }
+    }
+}
