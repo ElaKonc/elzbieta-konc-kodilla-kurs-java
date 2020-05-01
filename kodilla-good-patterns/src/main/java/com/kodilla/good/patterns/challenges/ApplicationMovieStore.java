2b23fee86a4e42d81c7073646d53ae2bdd75a7eb
Elzbieta <you@example.com>
2020-05-01 09:40:35 +0200

    dobre praktyki 1

---
+++ kodilla-good-patterns/src/main/java/com/kodilla/good/patterns/challenges/ApplicationMovieStore.java
@@ -0,0 +1,14 @@
+package com.kodilla.good.patterns.challenges;
+
+import java.util.stream.Collectors;
+
+public class ApplicationMovieStore {
+    public static void main(String[] args){
+        MovieStore movieStore = new MovieStore();
+
+        String movies = movieStore.getMovies().values().stream()
+                .flatMap(m -> m.stream())
+                .collect(Collectors.joining("!"));
+        System.out.println(movies);
+    }
+}
