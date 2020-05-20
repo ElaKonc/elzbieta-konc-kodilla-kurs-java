cd213d01ba3c25a6f139fef45fece0df605ea592
Elzbieta <you@example.com>
2020-05-20 14:54:03 +0200

    testSelectUsersAndPosts

--- kodilla-jdbc/src/test/java/com/kodilla/jdbc/DbManagerTestSuite.java
+++ kodilla-jdbc/src/test/java/com/kodilla/jdbc/DbManagerTestSuite.java
@@ -16,6 +16,7 @@ public class DbManagerTestSuite {
         //Then
         Assert.assertNotNull(dbManager.getConnection());
     }
+
     @Test
     public void testSelectUsers() throws SQLException {
         //Given
@@ -39,4 +40,27 @@ public class DbManagerTestSuite {
         Assert.assertEquals(5, counter);
     }
 
+    @Test
+    public void testSelectUsersAndPosts()throws SQLException{
+        //Given
+        DbManager dbManager = DbManager.getInstance();
+        //When
+        String sqlQuery = "select U.FIRSTNAME, U.LASTNAME, COUNT(P.ID) AS QUANTITY_OF_POSTS  from USERS U\n" +
+                "JOIN POSTS P\n" +
+                "ON U.ID=P.USER_ID\n" +
+                "GROUP BY U.LASTNAME, U.FIRSTNAME HAVING QUANTITY_OF_POSTS >= 2\n";
+        Statement statement = dbManager.getConnection().createStatement();
+        ResultSet rs = statement.executeQuery(sqlQuery);
+
+        //Then
+        int counter = 0;
+        while(rs.next()) {
+            System.out.println(rs.getString("FIRSTNAME") + ", " +
+                    rs.getString("LASTNAME"));
+            counter++;
+        }
+        rs.close();
+        statement.close();
+        Assert.assertEquals(2, counter);
+    }
 }
